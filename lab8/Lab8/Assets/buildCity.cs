0c3e81587af7ae321866c60ce3cae75c6b2e3914
YashVSolanki <38870520+YashVSolanki@users.noreply.github.com>
2020-05-25 22:40:22 -0700

    completed street tutorial

--- lab8/Lab8/Assets/buildCity.cs
+++ lab8/Lab8/Assets/buildCity.cs
@@ -6,36 +6,70 @@ public class buildCity : MonoBehaviour
 {
 
     public GameObject[] buildings;
+    public GameObject CrossRoad;
+    public GameObject XRoad;
+    public GameObject YRoad;
     public int mapWidth = 15;
     public int mapHeight = 15;
     public int buildingSpacing = 3;
+    int[,] mapgrid;
     public float noiseDivider = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        float seed = 256;//Random.Range(0, 1024);
-        for(int height = 0; height < mapHeight; height++)
+        float seed = Random.Range(0, 1024);
+        mapgrid = new int[mapWidth, mapHeight];
+        for (int height = 0; height < mapHeight; height++)
         {
-            for(int width = 0; width < mapWidth; width++)
+            for (int width = 0; width < mapWidth; width++)
+            {
+
+                mapgrid[width, height] = (int)(Mathf.PerlinNoise(width / noiseDivider + seed, height / noiseDivider + seed) * 10);
+
+
+            }
+        }
+        int x = 0;
+        for (int n = 0; n < 50; n++)
+        {
+            for (int height = 0; height < mapHeight; height++)
+            {
+                mapgrid[x, height] = -1;
+            }
+            x += 3;
+            if (x >= mapWidth) break;
+        }
+
+        int y = 0;
+        for (int n = 0; n < 10; n++)
+        {
+            for (int width = 0; width < mapWidth; width++)
+            {
+                if (mapgrid[width, y] == -1) mapgrid[width, y] = -3;
+                else mapgrid[width, y] = -2;
+
+            }
+            y += Random.Range(3, 8);
+            if (y >= mapHeight) break;
+        }
+
+
+
+        for (int height = 0; height < mapHeight; height++)
+        {
+            for (int width = 0; width < mapWidth; width++)
             {
                 Vector3 position = new Vector3(width * buildingSpacing, 0, height * buildingSpacing);
-                int randomBuildingNoise = (int)(Mathf.PerlinNoise(width/noiseDivider + seed, height/noiseDivider + seed) * 10);
-
-                if (randomBuildingNoise < 2) CreateBuilding(0, position);
-                else if (randomBuildingNoise < 4) CreateBuilding(1, position);
-                else if (randomBuildingNoise < 5) CreateBuilding(2, position);
-                else if (randomBuildingNoise < 7) CreateBuilding(3, position);
-                else if (randomBuildingNoise < 8)  CreateBuilding(4, position);
-                else if (randomBuildingNoise < 10) CreateBuilding(5, position);
+                if (mapgrid[width, height] < -2) Instantiate(CrossRoad, position, CrossRoad.transform.rotation);
+                else if (mapgrid[width, height] < -1) Instantiate(XRoad, position, XRoad.transform.rotation);
+                else if (mapgrid[width, height] < 0) Instantiate(YRoad, position, YRoad.transform.rotation);
+                else if (mapgrid[width, height] < 2) Instantiate(buildings[0], position, Quaternion.identity);
+                else if (mapgrid[width, height] < 4) Instantiate(buildings[1], position, Quaternion.identity);
+                else if (mapgrid[width, height] < 5) Instantiate(buildings[2], position, Quaternion.identity);
+                else if (mapgrid[width, height] < 7) Instantiate(buildings[3], position, Quaternion.identity);
+                else if (mapgrid[width, height] < 8) Instantiate(buildings[4], position, Quaternion.identity);
+                else if (mapgrid[width, height] < 10) Instantiate(buildings[5], position, Quaternion.identity);
             }
         }
     }
-
-
-    void CreateBuilding(int index, Vector3 localPosition)
-    {      
-        Instantiate(buildings[index], localPosition, Quaternion.identity);
-    }
-
-
 }
